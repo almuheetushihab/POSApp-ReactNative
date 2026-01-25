f2e641d0e96eda6160ec341197248b32aa628282
Md. AL MUHEETU <almuheetu@gmail.com>
2026-01-26 01:17:44 +0600

    refactor: reorganize layout imports and update configuration files

--- MyPosApp/app/_layout.tsx
+++ MyPosApp/app/_layout.tsx
@@ -1,17 +1,12 @@
-import {Stack} from 'expo-router';
 import "../src/global.css";
 import React from "react";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
     return (
-        <Stack
-            screenOptions={{
-                headerShown: false,
-                animation: 'slide_from_right',
-            }}
-        >
-            <Stack.Screen name="index"/>
-            <Stack.Screen name="dashboard"/>
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="dashboard" />
         </Stack>
     );
 }
