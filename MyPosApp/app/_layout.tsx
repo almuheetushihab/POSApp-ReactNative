1f838b19c5a5341702c4873532c96efe2d964fea
Md. AL MUHEETU <almuheetu@gmail.com>
2026-01-26 01:21:50 +0600

    feat: add slide animation to stack navigation in RootLayout

--- MyPosApp/app/_layout.tsx
+++ MyPosApp/app/_layout.tsx
@@ -1,10 +1,16 @@
 import "../src/global.css";
-import React from "react";
+
 import { Stack } from 'expo-router';
+import React from 'react';
 
 export default function RootLayout() {
     return (
-        <Stack screenOptions={{ headerShown: false }}>
+        <Stack
+            screenOptions={{
+                headerShown: false,
+                animation: 'slide_from_right',
+            }}
+        >
             <Stack.Screen name="index" />
             <Stack.Screen name="dashboard" />
         </Stack>
